43ba13ed63ecf2fdec3bb058cec8d6c2b0ceb019
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-12-21 22:35:34 +0530

    Initial backend setup for role based authentication

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import cors from 'cors';
+
+
+dotenv.config();
+connectDB();
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('API is running...');
+});
+
+const PORT= process.env.PORT || 4999;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
