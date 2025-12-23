0dd1fa1c50e6074ce3398b1b4335a60665991a98
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2025-12-23 21:39:51 +0530

    Implement user authentication with registration and login functionality

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,6 @@
 import User from '../models/User.js';
+import byrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const resgister=async(req,res)=>{
     try{
@@ -20,8 +22,10 @@ export const resgister=async(req,res)=>{
             }else{
          assignedRole = role === "admin" ? "admin" : "user";
        }
+       const salt= await byrypt.genSalt(10);
+        const hashedPassword= await byrypt.hash(password,salt);
        const newUser= await create({
-        email, password, role: assignedRole
+        email, password:hashedPassword, role: assignedRole
        });
        res.status(201).json({
         message: 'User registered successfully',
@@ -37,4 +41,38 @@ export const resgister=async(req,res)=>{
         console.error('Error during user registration:', error.message);
         res.status(500).json({ message: 'Server error' });
     }
+}
+
+export const Login =async(req,res)=>{
+
+const {email,password}=req.body;
+try{
+    const user = await user.findOne({email});
+    if(!user){
+        return res.status(400).json({message:'Invalid credentials'});
+    }
+    const isMatch= await byrypt.compare(password,user.password);
+    if(!isMatch){
+        return res.status(400).json({message:'Invalid credentials'});
+    }
+    // Generate JWT token
+    const token=jwt.sign({
+        userId:user._id,
+        role:user.role,
+    },
+    process.env.JWT_SECRET,
+    {expiresIn:process.env.JWT_EXPIRES_IN}
+    );
+    res.status(200).json({
+        message:'Login successful',
+        user:{
+            id:user._id,
+            email:user.email,
+            role:user.role,
+        }
+    });
+} catch(error){
+    console.error('Error during user login:', error.message);
+    res.status(500).json({message:'Server error'});
+}
 }
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import {register} from '../controllers/authController.js';
+import {register,Login} from '../controllers/authController.js';
 const router= express.Router();
 router.post('/register', register);
+router.post('/login', Login);
 export default router;
