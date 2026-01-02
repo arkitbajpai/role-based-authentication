2045169cdff6e87ff50a3eb3a8365d3214814926
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-01-02 17:33:26 +0530

    feat: implement user creation form in AdminDashboard

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import api from "../services/api.js";
+import { useState } from "react";
 
 
 const AdminDashboard = () => {
+    const [email, setEmail] = useState("");
+    const [password,setPassword]=useState("");
+    const [role,setRole]=useState("user");
   useEffect(() => {
     api.get("/admin/dashboard").catch(() => {
       window.location.href = "/login";
@@ -39,6 +43,26 @@ const handleLogout = async () => {
 return(
     <div>
         <h1>Admin Dashboard1 </h1>
+        <form onSubmit={handelCreateUser}>
+            <input
+                type="email"
+                placeholder="User Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="User Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <select value={role} onChange={(e) => setRole(e.target.value)}>
+                <option value="user">User</option>
+                <option value="admin">Admin</option>
+            </select>
+            <button type="submit">Create User</button>
+        </form>
+        <hr/>
         <button onClick={handleLogout}>Logout</button>
     </div>
 );
