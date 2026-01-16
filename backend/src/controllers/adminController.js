3269269d0de0d74d26fb31bf277fe8cefb8138dc
Arkit Bajpai <52690332+arkitbajpai@users.noreply.github.com>
2026-01-16 22:01:59 +0530

    feat: add user role management functionality in AdminDashboard

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -52,7 +52,7 @@ export const deleteUserByAdmin=async(req,res)=>{
     try{
         const userId=req.params.id;
         if(userId===req.user._id.toString()){
-            return res.status(400).json({ message: "Admin cannot delete self" });
+            return res.status(400).json({ message: "Admin cannot delete self sorry " });
         }
         await User.findByIdAndDelete(userId);
         res.status(200).json({ message: "User deleted successfully by admin" });
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -8,4 +8,4 @@ router.post('/login', Login);
 //   res.status(200).json({ message: `Welcome to the dashboard, ${req.user.email}` });
 // });
 router.post("/logout", logout);
-export default router
+export default router;
